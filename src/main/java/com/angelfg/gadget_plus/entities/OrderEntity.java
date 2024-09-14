4af0e686a64cd2eb15770ac4164afc96e341a614
Luis Angel <angelfgdeveloper@gmail.com>
2024-09-13 21:31:25 -0600

    OneToOne problema y solucion con el LAZY

--- src/main/java/com/angelfg/gadget_plus/entities/OrderEntity.java
+++ src/main/java/com/angelfg/gadget_plus/entities/OrderEntity.java
@@ -2,6 +2,7 @@ package com.angelfg.gadget_plus.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
@@ -20,8 +21,11 @@ public class OrderEntity {
     @Column(length = 32, nullable = false)
     private String clientName;
 
-    // OneToOne por defecto es Fetch.EAGER
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    // OneToOne por defecto es Fetch.EAGER y trae todo
+    // Si el OneToOne contiene FetchType.LAZY se genera un error de tipo: LazyInitializationException
+    // Si quiero utilizar el LAZY debo solo utilizar las variables que contiene este modelo y no el de la otra tabla
+    @ToString.Exclude // con el Lazy es para no llamar al ToString de la otra entity y salga el error
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "id_bill", nullable = false, unique = true) // Es la union esta entity con la otra tabla
     private BillEntity bill;
 
